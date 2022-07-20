f1d851ca23bd8efe0ea1a5f32bd4e88c760b3a32
Joshua Greer <jtgreer95@gmail.com>
2022-07-19 22:13:40 -0500

    Done with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function blurEyesOnMe(event){
+        console.log('Hey! Eyes on me!')
+    }
+    function focusEyesOnMe(event){
+        console.log("Good!")
+    }
+    return (
+        <div>
+            <button onBlur={blurEyesOnMe} onFocus={focusEyesOnMe}>Eyes on me</button>
+            Super Safe Password Stuff</div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
+
+function Keypad() {
+    function handleKeypad(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input
+            type="password"
+            onChange= {handleKeypad}
+            placeholder="Password"
+            ></input>
+        </div>
     )
 }
 
